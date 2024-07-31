76d0b2823db9b8647d976625dd5eef2005889787
alancj731 <alanchenjian@gmail.com>
2024-07-30 22:50:52 -0500

    add icons of linked in and github

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+	import '@fortawesome/fontawesome-free/css/all.min.css';
+
 	export let bgText = '';
 	onMount(() => {
-		const smallDisplay: boolean = window.matchMedia('(max-width: 600px)').matches;
+		const smallDisplay: boolean = window.matchMedia('(max-width: 768px)').matches;
 
 		bgText = smallDisplay
 			? '        Code never lies,\ncomments sometimes do.'
@@ -11,11 +13,29 @@
 	});
 </script>
 
-<div class="grid h-full w-full items-center justify-center p-0">
+<svelte:head>
+	<title>Jian's Portfolio</title>
+</svelte:head>
+<div class="flex h-full w-full flex-col items-center justify-center p-0">
 	<pre class="background-text playwrite-ar-bg leading-loose text-white">{bgText}</pre>
+	<div class="icon-container flex justify-center space-x-6">
+		<a href="https://www.linkedin.com/in/alanchenjian/">
+			<i class="icon fa-brands fa-linkedin-in invert"></i>
+		</a>
+		<a href="https://github.com/alancj731"><i class="icon fa-brands fa-github invert"></i></a>
+	</div>
 </div>
 
 <style>
+
+    .icon {
+        transition: all 0.5s linear;
+    }
+    .icon:hover {
+        filter: none;
+        scale: 1.1;
+        transition: all 0.5s linear;
+    }
 	@keyframes fadeout {
 		0% {
 			opacity: 1;
@@ -28,26 +48,48 @@
 
 	.background-text {
 		font-size: 7vw;
+		position: absolute;
+		top: 36%;
 		animation: fadeout 3s linear forwards;
-		transition:
-			all 0.8s linear;
+		transition: all 0.8s linear;
 	}
 	.background-text:hover {
 		animation: none;
+        color: black;
 		opacity: 1;
 		transform: scale(1.05);
-        transition: 
-            all 0.5s linear;
+		transition: all 0.5s linear;
+	}
+	.icon-container {
+		position: absolute;
+		top: 88%;
+		font-size: 1.5rem;
 	}
 
-	@media screen and (max-width: 768px) {
+	/* @media screen and (max-width: 401px) {
 		.background-text {
-			font-size: 4.3vw;
+            position: absolute;
+            top: 35%;
+			font-size: 7vw;
+		}
+	} */
+
+	@media screen and (min-width: 400px) {
+		.background-text {
+			top: 38%;
+			font-size: 6vw;
 		}
 	}
-	@media screen and (min-width: 769px) {
+	@media screen and (min-width: 768px) {
 		.background-text {
-			font-size: 4.3vw;
+			top: 36%;
+			font-size: 5.5vw;
+		}
+	}
+	@media screen and (min-width: 1024px) {
+		.background-text {
+			top: 40%;
+			font-size: 4vw;
 		}
 	}
 </style>
